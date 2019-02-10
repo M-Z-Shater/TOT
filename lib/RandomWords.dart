31930809a6b037a7eb4fd5fc290ccf620b4d75b8
shater <mohamed.zakria.shater@gmail.com>
2019-02-10 21:54:50 +0200

    TOT commit last with snackbar

---
+++ lib/RandomWords.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:tot_app/RandomWordsState.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
---
+++ lib/RandomWordsState.dart
@@ -0,0 +1,100 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:tot_app/RandomWords.dart';
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  BuildContext _buildContext;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          _buildContext = _context;
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    String title = pair.asPascalCase;
+    return new ListTile(
+      title: new Text(
+        title,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+            final snackBar = SnackBar(
+                content: Text(title + "    removed"),
+                duration: new Duration(seconds: 1));
+            Scaffold.of(_buildContext).showSnackBar(snackBar);
+          } else {
+            _saved.add(pair);
+            final snackBar = SnackBar(
+                content: Text(title + "      added"),
+                duration: new Duration(seconds: 1));
+            Scaffold.of(_buildContext).showSnackBar(snackBar);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+}
